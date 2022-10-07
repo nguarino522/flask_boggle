f44ca4be41bc03241d13af4357f52fab74ed5248
nguarino522 <sub4miler5@gmail.com>
2022-10-07 16:58:49 -0400

    step 4 complete, on step 5

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from boggle import Boggle
-from flask import Flask, request, render_template, redirect, flash, session
+from flask import Flask, request, render_template, redirect, flash, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -9,8 +9,40 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 boggle_game = Boggle()
 
+
 @app.route("/")
 def default_route():
-    """default base template route, etc."""
-    
-    return render_template("game.html")
+    """default base template route and homepage"""
+
+    homepage = True
+    return render_template("index.html", homepage=homepage)
+
+
+@app.route("/game")
+def game_route():
+    """route once game is started"""
+
+    board = boggle_game.make_board()
+    session['board'] = board
+
+    return render_template("game.html", board=board)
+
+
+@app.route("/wordcheck")
+def word_check():
+    """checking to see if word exists"""
+
+    word = request.args["word"]
+    board = session["board"]
+    result = jsonify(boggle_game.check_valid_word(board, word))
+
+    return result
+
+
+@app.route("/score")
+def score_update():
+    """handling of scoring for game"""
+
+    score = request.json["score"]
+
+    return
--- static/app.js
+++ static/app.js
@@ -1,5 +1,44 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+    let score = 0;
 
-console.log("test");
+    document.getElementById("checkwordbtn").addEventListener("click", function(e){
+        e.preventDefault();
+        
+        word = $('#word').val();
+        if (word.length === 0 ){
+            alert("Enter a word please");
+        } else {
+            sendWordToServer(word);
+        }
+        $('#word').val('');
+    });
 
+    async function sendWordToServer(word){
+        let resp = await axios.get("/wordcheck", { params: { word: word }});
+        console.log("got", resp);
+        if (resp.data === "not-word"){
+            showMsg(`The word ${word} is not found in the application's dictionary.`, "err");
+        } else if (resp.data === "not-on-board"){
+            showMsg(`The word ${word} was not found on current board.`, "warn");
+        } else {
+            showMsg(`Success! The word ${word} was found.`, "ok");
+            score += word.length;
+            $(".score").text(score);
+        }
+    }
+
+    function showMsg(msg, lvl){
+        let msgDiv = document.getElementById("msgdiv");
+        $("#msgdiv").empty();
+        let newP = document.createElement("p");
+        newP.classList.add("msg", `${lvl}`);
+        newP.textContent = msg;
+        msgDiv.appendChild(newP);
+        
+        //$(".msg").text(msg).removeClass().addClass(`msg ${lvl}`);
+    }
+
+    async function sendScoreToSever(score){
+        let resp = await axios.post("/score")
+    }
 });
